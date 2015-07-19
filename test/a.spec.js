539f968df6b48bc3ce2c5ada62e3c86e9613318b
Leo Picado <leopic@gmail.com>
2015-07-19 12:27:35 -0600

    Adding simple test.

---
+++ test/a.spec.js
@@ -0,0 +1,12 @@
+describe('A test suite', function() {
+  var expect = window.expect;
+
+  beforeEach(function() { });
+
+  afterEach(function() { });
+
+  it('should work?', function() {
+    expect(true).to.be.equal(false);
+  });
+});
+
